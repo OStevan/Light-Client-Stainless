1acc7736cc1296dbdb767417a55afae32812a2af
Stevan <ognjanovic.stevan1@gmail.com>
2020-03-03 17:14:29 +0100

    Full max..

--- verified/src/main/scala/test/TestMax.scala
+++ verified/src/main/scala/test/TestMax.scala
@@ -5,17 +5,22 @@ import stainless.collection._
 import stainless.annotation._
 
 object TestMax {
-  def max(x: Int, y: Int): Int = {
-    require(x >= 0 && y >= 0)
+  def max(x: BigInt, y: BigInt): BigInt = {
     val d = x - y
     if (d > 0) x
     else y
   } ensuring (res => x <= res && y <= res && (res == x || res == y))
 
-  def min(x: Int, y: Int): Int = {
-    require(x >= 0 && y >= 0)
+  def min(x: BigInt, y: BigInt): BigInt = {
     val diff = x - y
     if (diff > 0) y
     else x
   } ensuring (res => x >= res && y >= res && (res == x || res == y))
+
+  def maxLemma(x: BigInt, y: BigInt, z: BigInt) = {
+    max(x,x) == x &&
+    max(x,y) == max(y,x) &&
+    max(x,max(y,z)) == max(max(x,y), z) &&
+    max(x,y) + z == max(x + z, y + z)
+  }.holds
 }
