ac082183e690148368158204de9e2569ac7c3e26
Stevan <ognjanovic.stevan1@gmail.com>
2020-03-11 13:48:27 +0100

    First verified power of set

--- verified/src/main/scala/ch/epfl/ognjanovic/stevan/library/Validators.scala
+++ verified/src/main/scala/ch/epfl/ognjanovic/stevan/library/Validators.scala
@@ -2,30 +2,26 @@ package ch.epfl.ognjanovic.stevan.library
 
 import stainless.lang._
 import stainless.collection._
-
+import stainless.annotation._
 import ch.epfl.ognjanovic.stevan.library.types.VotingPowers._
 import ch.epfl.ognjanovic.stevan.library.Nodes.Node
+import ch.epfl.ognjanovic.stevan.library.types.VotingPowers
 
 case class Validators(validators: Map[Node, PositiveVotingPower]) {
+    val totalPower: VotingPower = validators.values.
+      foldLeft(VotingPowers.zero())((acc, value) => acc + value)
 
-    def setPower(nodes: Set[Node]): VotingPower = {
-        require(nodes subsetOf validators.keys.toSet)
-        nodePower(nodes.toList)
+    def obtainedByzantineQuorum(nodes: List[Node]): Boolean = {
+        require((nodes.content.toList.size == nodes.size) && (nodes.content subsetOf validators.keys.content))
+        nodesPower(nodes) * 3 > totalPower * 2
     }
 
-    // def obtainedByzantineQuorum(nodes: Set[Node]): Boolean = {
-    //     // require(nodes subsetOf validators.keys.toSet)
-    //     // val totalPower = ZeroVotingPower
-    //     // validators.values
-    //         // .foldLeft(ZeroVotingPower.asInstanceOf[VotingPower])((first, second) => first + second)
-    //     // val votedPower = setPower(nodes)
-    //     // (votedPower.*(3)) > (totalPower.*(2))
-    //     true
-    // }
-
-    def nodePower(nodes: List[Node]): VotingPower = {
+    def nodesPower(nodes: List[Node]): VotingPower = {
+        require(nodes.content subsetOf validators.keys.content)
         nodes match {
-            case Cons(head, tail) => validators.get(head).power().+(nodePower(tail))
+            case Cons(head, tail) => 
+                assert(validators.contains(head))
+                validators.get(head).power + nodesPower(tail)
             case Nil() => ZeroVotingPower 
         }
     }
--- verified/src/main/scala/ch/epfl/ognjanovic/stevan/library/types/VotingPower.scala
+++ verified/src/main/scala/ch/epfl/ognjanovic/stevan/library/types/VotingPower.scala
@@ -4,51 +4,71 @@ import stainless.lang._
 import stainless.annotation._
 
 object VotingPowers {
-    sealed abstract class VotingPower {
-      def power(): BigInt
+  def zero(): VotingPower = ZeroVotingPower
+  def positive(power: BigInt): VotingPower = {
+    require(power > 0)
+    PositiveVotingPower(power)
+  }
 
-      def +(other: VotingPower): VotingPower
+  sealed abstract class VotingPower {
+    def power(): BigInt
 
-    //   def *(multiplier: Int): VotingPower
+    def +(other: VotingPower): VotingPower
 
-      def >(other: VotingPower): Boolean
-
-      @law
-      def positive_power(): Boolean = {
-          power() >= BigInt(0)
+    def *(multiplier: BigInt): VotingPower = {
+      require(multiplier >= BigInt(0))
+      multiplier match {
+        case a if a == BigInt(0) => ZeroVotingPower
+        case _ => this match {
+          case PositiveVotingPower(value) => PositiveVotingPower(multiplier * value)
+          case ZeroVotingPower => ZeroVotingPower
+        }
       }
+    }
+
+    def >(other: VotingPower): Boolean
+
+    def >=(other: VotingPower): Boolean
+
+    @law
+    def positive_power(): Boolean = {
+      power() >= BigInt(0)
+    }
   }
 
   case object ZeroVotingPower extends VotingPower {
-      def power(): BigInt = BigInt(0)
-      def +(other: VotingPower): VotingPower = other
-    //   def *(multiplier: Int): VotingPower = this
-      def >(other: VotingPower): Boolean = false
+    def power(): BigInt = BigInt(0)
+
+    override def +(other: VotingPower): VotingPower = other
+
+    override def >(other: VotingPower): Boolean = false
+
+    override def >=(other: VotingPower): Boolean = this > other || this == other
   }
 
   case class PositiveVotingPower(value: BigInt) extends VotingPower {
-      require(value > BigInt(0))
+    require(value > BigInt(0))
 
-      def power(): BigInt = value
+    override def power(): BigInt = value
 
-      def +(other: VotingPower): VotingPower = other match {
-          case PositiveVotingPower(power) => PositiveVotingPower(power + value)
-          case ZeroVotingPower => this
-      }
+    override def +(other: VotingPower): VotingPower = other match {
+      case PositiveVotingPower(power) => PositiveVotingPower(power + value)
+      case ZeroVotingPower => this
+    }
 
-    //   def *(multiplier: Int): VotingPower = PositiveVotingPower(value * BigInt(multiplier))
+    override def >(other: VotingPower): Boolean = other match {
+      case PositiveVotingPower(value) => power() > value
+      case ZeroVotingPower => true
+    }
 
-      def >(other: VotingPower): Boolean = other match {
-          case PositiveVotingPower(value) => power() > value
-          case ZeroVotingPower => true
-      }
+    override def >=(other: VotingPower): Boolean = this == other || this > other
   }
 
   @library
   implicit class VotingPowerOptional(val votingPower: Option[PositiveVotingPower]) extends AnyVal {
-      def power(): VotingPower = votingPower match {
-          case Some(value) => value
-          case None() => ZeroVotingPower
-      }
+    def power(): VotingPower = votingPower match {
+      case Some(value) => value
+      case None() => ZeroVotingPower
+    }
   }
 }
