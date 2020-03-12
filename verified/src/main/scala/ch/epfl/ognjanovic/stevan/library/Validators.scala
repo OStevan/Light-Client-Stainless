3686837bf170b750960e81fa800c53ec3aa2452c
Stevan <ognjanovic.stevan1@gmail.com>
2020-03-12 15:51:08 +0100

    With accumulator.

--- verified/src/main/scala/ch/epfl/ognjanovic/stevan/library/Validators.scala
+++ verified/src/main/scala/ch/epfl/ognjanovic/stevan/library/Validators.scala
@@ -12,18 +12,15 @@ case class Validators(validators: Map[Node, PositiveVotingPower]) {
       foldLeft(VotingPowers.zero())((acc, value) => acc + value)
 
     def obtainedByzantineQuorum(nodes: List[Node]): Boolean = {
-        require((nodes.content.toList.size == nodes.size) && (nodes.content subsetOf validators.keys.content))
-        nodesPower(nodes) * VotingPowers.positive(3) > totalPower * VotingPowers.positive(2)
+        require(nodes.content.toList.size == nodes.size)
+        nodesPower(nodes, ZeroVotingPower) * VotingPowers.positive(3) > totalPower * VotingPowers.positive(2)
     }
 
-    @induct
-    def nodesPower(nodes: List[Node]): VotingPower = {
-        require(nodes.content subsetOf validators.keys.content)
+    def nodesPower(nodes: List[Node], acc: VotingPower): VotingPower = {
+        require(nodes.content.toList.size == nodes.size && totalPower >= acc)
         nodes match {
-            case Cons(head, tail) => 
-                assert(validators.contains(head))
-                validators.get(head).power + nodesPower(tail)
-            case Nil() => ZeroVotingPower 
+            case Cons(head, tail) => nodesPower(tail, acc + validators.get(head).power)
+            case _ => acc
         }
-    }
+    } ensuring(res => totalPower >= res)
 }
