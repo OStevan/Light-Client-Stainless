40a51c51c1c72d86bf925160b6aa5428c6c51208
Stevan <ognjanovic.stevan1@gmail.com>
2020-03-13 14:13:43 +0100

    Quorom check when we have faulty nodes

--- verified/src/main/scala/ch/epfl/ognjanovic/stevan/library/Validators.scala
+++ verified/src/main/scala/ch/epfl/ognjanovic/stevan/library/Validators.scala
@@ -8,13 +8,18 @@ import ch.epfl.ognjanovic.stevan.library.types.VotingPower
 import ch.epfl.ognjanovic.stevan.library.types.NodePowers
 
 case class Validators(validators: NodePowers) {
+
+    def nodesPower(nodes: Set[Node]): VotingPower = {
+        require(nodes subsetOf validators.keys)
+        nodes.toList.foldLeft(VotingPower(0))((acc, value) => acc + validators(value))
+    }
+
     def obtainedByzantineQuorum(nodes: Set[Node]): Boolean = {
         require(nodes subsetOf validators.keys)
         nodesPower(nodes) * VotingPower(3) > validators.totalPower * VotingPower(2)
     }
 
-    def nodesPower(nodes: Set[Node]): VotingPower = {
-        require(nodes subsetOf validators.keys)
-        nodes.toList.foldLeft(VotingPower(0))((acc, value) => acc + validators(value))
+    def isCorrect(faultyNodes: Set[Node]): Boolean = {
+        nodesPower(validators.keys -- faultyNodes) > nodesPower(validators.keys & faultyNodes) * VotingPower(2)
     }
 }
