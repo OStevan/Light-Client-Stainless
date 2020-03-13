2618e4cec795102aca849262bbae9cc6b7060774
Stevan <ognjanovic.stevan1@gmail.com>
2020-03-13 11:06:55 +0100

    Simple changes

--- verified/src/main/scala/ch/epfl/ognjanovic/stevan/library/Validators.scala
+++ verified/src/main/scala/ch/epfl/ognjanovic/stevan/library/Validators.scala
@@ -7,15 +7,14 @@ import ch.epfl.ognjanovic.stevan.library.Nodes.Node
 import ch.epfl.ognjanovic.stevan.library.types.VotingPower
 import ch.epfl.ognjanovic.stevan.library.types.NodePowers
 
-case class Validators(nodePowers: NodePowers) {
-
+case class Validators(validators: NodePowers) {
     def obtainedByzantineQuorum(nodes: Set[Node]): Boolean = {
-        require(nodes subsetOf nodePowers.keys.content)
-        nodesPower(nodes) * VotingPower(3) > nodePowers.totalPower * VotingPower(2)
+        require(nodes subsetOf validators.keys)
+        nodesPower(nodes) * VotingPower(3) > validators.totalPower * VotingPower(2)
     }
 
     def nodesPower(nodes: Set[Node]): VotingPower = {
-        require(nodes subsetOf nodePowers.keys.content)
-        nodes.toList.map(key => nodePowers(key)).foldLeft(VotingPower(0))((acc, value) => acc + value)
-    } ensuring(res => nodePowers.totalPower >= res)
+        require(nodes subsetOf validators.keys)
+        nodes.toList.foldLeft(VotingPower(0))((acc, value) => acc + validators(value))
+    } ensuring(res => validators.totalPower >= res)
 }
--- verified/src/main/scala/ch/epfl/ognjanovic/stevan/library/types/NodePowers.scala
+++ verified/src/main/scala/ch/epfl/ognjanovic/stevan/library/types/NodePowers.scala
@@ -7,19 +7,16 @@ import stainless.collection._
 
 case class NodePowers(totalPower: VotingPower, powerAssigments: Map[Node, VotingPower]) {
     require(powerAssigments.values.forall(value => value.isPositive) && totalPower == powerAssigments.values.foldLeft(VotingPower(0))((acc, value) => acc + value))
-
-    @inlineOnce
-    def nodes = powerAssigments.keys.content
     
     def nodePower(node: Node): VotingPower = {
         powerAssigments.getOrElse(node, VotingPower(0))
     } ensuring(res => (contains(node) ==> res.isPositive) || ((!contains(node)) ==> (!res.isPositive)))
 
-    def keys: List[Node] = powerAssigments.keys
+    def keys: Set[Node] = powerAssigments.keys.content
 
     def values: List[VotingPower] = powerAssigments.values
 
-    def contains(node: Node) = nodes.contains(node)
+    def contains(node: Node) = keys.contains(node)
 
     def apply(node: Node) = this.nodePower(node)
 }
