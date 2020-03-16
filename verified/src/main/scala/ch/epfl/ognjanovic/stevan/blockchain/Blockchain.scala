08c3d40c7aa4b2c0b7d9e0c5868bfd7e7ea93c20
Stevan <ognjanovic.stevan1@gmail.com>
2020-03-16 12:47:06 +0100

    Improve Blockchain and BlockHeader

--- verified/src/main/scala/ch/epfl/ognjanovic/stevan/blockchain/Blockchain.scala
+++ verified/src/main/scala/ch/epfl/ognjanovic/stevan/blockchain/Blockchain.scala
@@ -1,5 +1,22 @@
 package ch.epfl.ognjanovic.stevan.blockchain
 
-case class Blockchain() {
-  
+import ch.epfl.ognjanovic.stevan.library.types.Height
+import ch.epfl.ognjanovic.stevan.library.Nodes._
+import ch.epfl.ognjanovic.stevan.library._
+import stainless.lang._
+import stainless.collection._
+import stainless.math._
+
+case class Blockchain(tooManyFaults: Boolean, height: Height, minTrustedHeight: Height, chain: List[BlockHeader], faulty: Set[Node]) {
+    require(chain.nonEmpty && chain.size == height.value + BigInt(1))
+
+    def chainFault(): Boolean = {
+        chain.slice(min(minTrustedHeight.value, chain.length), chain.length).forall(header => header.nextValidatorSet.isCorrect(faulty))
+    }
+
+    def appendBlock(header: BlockHeader): Blockchain = {
+        Blockchain(tooManyFaults, Height(height.value + BigInt(1)), minTrustedHeight, header :: chain, faulty)
+    }
+
+    def size(): BigInt = chain.size
 }
--- verified/src/main/scala/ch/epfl/ognjanovic/stevan/library/BlockHeader.scala
+++ verified/src/main/scala/ch/epfl/ognjanovic/stevan/library/BlockHeader.scala
@@ -2,5 +2,6 @@ package ch.epfl.ognjanovic.stevan.library
 
 import stainless.lang._
 import ch.epfl.ognjanovic.stevan.library.Nodes._
+import ch.epfl.ognjanovic.stevan.library.types.Height
 
-case class BlockHeader(height: Int, commit: Set[Node], validatorSet: Validators, nextValidatorSet: Validators)
+case class BlockHeader(height: Height, lastCommit: Set[Node], validatorSet: Validators, nextValidatorSet: Validators)
