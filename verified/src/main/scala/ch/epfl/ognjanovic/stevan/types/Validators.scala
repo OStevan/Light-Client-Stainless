2246a89257ef8bdb94ea6f2c558014427d83a0a0
Stevan <ognjanovic.stevan1@gmail.com>
2020-05-03 17:39:11 +0200

    Improved invariants.

--- verified/src/main/scala/ch/epfl/ognjanovic/stevan/types/Validators.scala
+++ verified/src/main/scala/ch/epfl/ognjanovic/stevan/types/Validators.scala
@@ -1,56 +1,67 @@
 package ch.epfl.ognjanovic.stevan.types
 
 import ch.epfl.ognjanovic.stevan.types.Nodes.Node
-import stainless.annotation.{extern, pure}
+import stainless.annotation.{extern, induct, opaque, pure}
 import stainless.collection._
 import stainless.lang._
-import utils.ListMap
+import stainless.proof._
+import utils.ListSetUtils._
+import utils.{ListMap, ListSetUtils, ListUtils}
 
 case class Validators(totalPower: VotingPower, powerAssignments: ListMap[Node, VotingPower]) {
   require(powerAssignments.forall(value => value._2.isPositive) &&
-    totalPower == powerAssignments.toList.foldLeft(VotingPower(0))((acc, value) => acc + value._2) &&
+    totalPower == Validators.sumVotingPower(powerAssignments.toList) &&
     !powerAssignments.isEmpty)
 
-  def nodePower(node: Node): VotingPower = {
-    if (powerAssignments.contains(node))
-      powerAssignments(node)
-    else
-      VotingPower(0)
-  } ensuring (res => (contains(node) ==> res.isPositive) || ((!contains(node)) ==> (!res.isPositive)))
-
+  @pure @extern
   def keys: Set[Node] = powerAssignments.toList.map(_._1).content
 
   def values: List[VotingPower] = powerAssignments.toList.map(_._2)
 
   def contains(node: Node): Boolean = keys.contains(node)
 
-  def apply(node: Node): VotingPower = {
-    nodePower(node)
-  }
-
-  def nodesPower(nodes: Set[Node]): VotingPower = {
-    require(nodes subsetOf keys)
-    nodes.toList.foldLeft(VotingPower(0))((acc, value) => acc + apply(value))
+  def nodesPower(nodes: List[Node]): VotingPower = {
+    require(nodes.forall(powerAssignments.toList.map(_._1).contains))
+    Validators.sumVotingPower(powerAssignments.toList.filter(pair => nodes.contains(pair._1)))
   }
 
   @pure
   def obtainedByzantineQuorum(nodes: Set[Node]): Boolean = {
     require(nodes subsetOf keys)
-    nodesPower(nodes) * VotingPower(3) > totalPower * VotingPower(2)
+    val nodeList = Validators.nodeListContainment(nodes, this)
+    nodesPower(nodeList) * VotingPower(3) > totalPower * VotingPower(2)
   }
 
   @pure
   def isCorrect(faultyNodes: Set[Node]): Boolean = {
-    nodesPower(keys -- faultyNodes) > nodesPower(keys & faultyNodes) * VotingPower(2)
+    val keySet = powerAssignments.toList.map(_._1)
+    val faultyNodesSet = faultyNodes.toList
+    val difference = removingFromSet(keySet, faultyNodesSet)
+    val intersection = keySet & faultyNodesSet
+    setIntersectionLemma(keySet, faultyNodesSet)
+    nodesPower(difference) > nodesPower(intersection) * VotingPower(2)
   }
 
   def checkSupport(nodes: Set[Node]): Boolean = {
     require(nodes subsetOf keys)
-    VotingPower(3) * nodesPower(nodes) > nodesPower(keys)
+    val nodeList = Validators.nodeListContainment(nodes, this)
+    ListSetUtils.selfContainment(powerAssignments.toList.map(_._1))
+    VotingPower(3) * nodesPower(nodeList) > totalPower
   }
 }
 
 object Validators {
+
+  def sumVotingPower(votingPowers: List[(Node, VotingPower)]): VotingPower = votingPowers match {
+    case Nil() => VotingPower(0)
+    case Cons(head, tail) => head._2 + sumVotingPower(tail)
+  }
+
+  @extern
+  private def nodeListContainment(set: Set[Node], validators: Validators): List[Node]= {
+    require(set subsetOf validators.keys)
+    set.toList
+  }.ensuring(res => res.forall(validators.powerAssignments.toList.map(_._1).contains))
 
   @extern
   def moreFaultyDoesNotHelp(current: Set[Node], next: Set[Node]): Unit = {
