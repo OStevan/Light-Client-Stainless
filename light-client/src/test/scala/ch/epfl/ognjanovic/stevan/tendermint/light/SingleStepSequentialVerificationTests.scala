beab7e6edd919bd8aa87c397c35c877f6fbe0d5b
Stevan <ognjanovic.stevan1@gmail.com>
2020-05-26 18:14:17 +0200

    Added one single step test.

---
+++ light-client/src/test/scala/ch/epfl/ognjanovic/stevan/tendermint/light/SingleStepSequentialVerificationTests.scala
@@ -0,0 +1,46 @@
+package ch.epfl.ognjanovic.stevan.tendermint.light
+
+import java.time.Instant
+
+import ch.epfl.ognjanovic.stevan.tendermint.rpc.circe.CirceDeserializer
+import ch.epfl.ognjanovic.stevan.tendermint.verified.light.NextHeightCalculators.SequentialHeightCalculator
+import ch.epfl.ognjanovic.stevan.tendermint.verified.light.VerificationOutcomes.Success
+import ch.epfl.ognjanovic.stevan.tendermint.verified.light.VerifierStates.{Finished, WaitingForHeader}
+import ch.epfl.ognjanovic.stevan.tendermint.verified.light.{TrustVerifiers, TrustedState, UntrustedState, Verifier}
+import ch.epfl.ognjanovic.stevan.tendermint.verified.types.Height
+import org.scalatest.flatspec.AnyFlatSpec
+
+sealed class SingleStepSequentialVerificationTests extends AnyFlatSpec {
+
+  "Verifying a block with the same validator sets" should "succeed" in {
+    val content = LightClientIntegrationTests.content(
+      "/single-step/sequential/validator_set/1_validator.json")
+    val (trustedHeader, trustingPeriod, now, provider) =
+      new CirceDeserializer(singleStepTestCaseDecoder)(content)
+
+    val verifier = SingleStepSequentialVerificationTests.createVerifierWithDefaultTrustLevel(trustingPeriod, now)
+
+    val requestHeight = Height(2)
+    val result = verifier.processHeader(
+      WaitingForHeader(
+        requestHeight,
+        requestHeight,
+        TrustedState(trustedHeader, TrustVerifiers.defaultTrustVerifier),
+        UntrustedState.empty),
+      provider.lightBlock(requestHeight))
+
+    assert(result.isInstanceOf[Finished])
+    assert(result.asInstanceOf[Finished].outcome == Success)
+  }
+
+}
+
+object SingleStepSequentialVerificationTests {
+  private def createVerifierWithDefaultTrustLevel(trustingPeriod: Long, now: Instant) = {
+    val verifier = Verifier(
+      new TimeBasedExpirationChecker(() => now, trustingPeriod),
+      TrustVerifiers.defaultTrustVerifier,
+      SequentialHeightCalculator)
+    verifier
+  }
+}
