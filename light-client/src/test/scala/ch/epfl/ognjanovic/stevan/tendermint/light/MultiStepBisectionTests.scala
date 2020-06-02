c53651825b6cc9d32deb3fe47d17d23419e7e552
Stevan <ognjanovic.stevan1@gmail.com>
2020-06-02 11:31:51 +0200

    Expired trusted state test.

--- light-client/src/test/scala/ch/epfl/ognjanovic/stevan/tendermint/light/MultiStepBisectionTests.scala
+++ light-client/src/test/scala/ch/epfl/ognjanovic/stevan/tendermint/light/MultiStepBisectionTests.scala
@@ -1,7 +1,7 @@
 package ch.epfl.ognjanovic.stevan.tendermint.light
 
 import ch.epfl.ognjanovic.stevan.tendermint.verified.light.UntrustedStates
-import ch.epfl.ognjanovic.stevan.tendermint.verified.light.VerificationOutcomes.Success
+import ch.epfl.ognjanovic.stevan.tendermint.verified.light.VerificationOutcomes._
 import org.scalatest.flatspec.AnyFlatSpec
 
 sealed class MultiStepBisectionTests extends AnyFlatSpec {
@@ -12,5 +12,14 @@ sealed class MultiStepBisectionTests extends AnyFlatSpec {
     val result = verifier.verifyUntrusted(trustedState, UntrustedStates.empty(heightToVerify))
 
     assert(result == Success)
+  }
+
+  "Trusted state expired" should "fail verification" in {
+    val (verifier, trustedState, heightToVerify) = MultiStepVerifierTests.deserializeMultiStepTestCase(
+      "/bisection/single-peer/header_out_of_trusting_period.json")
+
+    val result = verifier.verifyUntrusted(trustedState, UntrustedStates.empty(heightToVerify))
+
+    assert(result == ExpiredTrustedState)
   }
 }
