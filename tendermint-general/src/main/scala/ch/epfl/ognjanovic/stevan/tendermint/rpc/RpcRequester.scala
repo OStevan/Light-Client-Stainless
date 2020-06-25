d073eb828171fa7abe631cf6884549322780c6d2
Stevan <ognjanovic.stevan1@gmail.com>
2020-06-25 11:45:57 +0200

    RpcRequester implementation.

--- tendermint-general/src/main/scala/ch/epfl/ognjanovic/stevan/tendermint/rpc/RpcRequester.scala
+++ tendermint-general/src/main/scala/ch/epfl/ognjanovic/stevan/tendermint/rpc/RpcRequester.scala
@@ -1,6 +1,8 @@
 package ch.epfl.ognjanovic.stevan.tendermint.rpc
 import ch.epfl.ognjanovic.stevan.tendermint.verified.types.{Height, PeerId, ValidatorSet}
+import stainless.annotation.ignore
 
+@ignore
 class RpcRequester(val peerId: PeerId, nodeClient: TendermintFullNodeClient) extends Requester {
   override def signedHeader(height: Option[Height]): SignedHeader = nodeClient.commit(height)
 
