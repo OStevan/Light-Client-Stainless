98e915a2693ab024cb931a12b19ec259173a2255
Stevan <ognjanovic.stevan1@gmail.com>
2020-07-01 17:44:30 +0200

    Some small refactorings.

--- light-client/src/main/scala/ch/epfl/ognjanovic/stevan/tendermint/light/EventLoopClient.scala
+++ light-client/src/main/scala/ch/epfl/ognjanovic/stevan/tendermint/light/EventLoopClient.scala
@@ -11,13 +11,12 @@ import ch.epfl.ognjanovic.stevan.tendermint.verified.light.VotingPowerVerifiers.
 import ch.epfl.ognjanovic.stevan.tendermint.verified.types.{Duration, Height, LightBlock}
 import stainless.lang
 
+import scala.annotation.tailrec
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService, Future}
 
 object EventLoopClient {
 
-  /**
-   * Not a real implementation just a sketch of how it should look like
-   */
+  // TODO add evidence reporter, implement real errors for supervisor
   class EventLoopSupervisor(
     @volatile private var peerList: PeerList,
     private val votingPowerVerifier: VotingPowerVerifier,
@@ -26,16 +25,72 @@ object EventLoopClient {
     private var trustedState: TrustedState, // currently var, but should be changed with store and recreate a trusted state for verification
     private val forkDetector: ForkDetector)
       extends Supervisor {
-    // TODO add evidence reporter
 
-    // TODO probably have to change error
-    override def verifyToHeight(height: Height): Either[LightBlock, Supervisor.Error] = verifyToTarget(Some(height))
+    override def verifyToHeight(height: Height): Either[LightBlock, Supervisor.Error] = {
+      val (newPeerList, result) = verifyToTarget(Some(height), peerList)
+      peerList = newPeerList
+      result
+    }
 
-    override def verifyToHighest(): Either[LightBlock, Supervisor.Error] = verifyToTarget(None)
+    override def verifyToHighest(): Either[LightBlock, Supervisor.Error] = {
+      val (newPeerList, result) = verifyToTarget(None, peerList)
+      peerList = newPeerList
+      result
+    }
 
     override def handle: Handle = {
       val executorContext = ExecutionContext.fromExecutorService(Executors.newSingleThreadExecutor())
       new EventLoopHandle(this, executorContext)
+    }
+
+    @tailrec
+    private def verifyToTarget(
+      height: Option[Height],
+      peerList: PeerList): (PeerList, Either[LightBlock, Supervisor.Error]) = {
+      val primaryVerifier = verifierBuilder.constructVerifier(peerList.primary, votingPowerVerifier, trustDuration)
+
+      val primaryResult =
+        primaryVerifier.verifyUntrusted(
+          trustedState,
+          UntrustedStates.empty(height.getOrElse(peerList.primary.currentHeight)))
+
+      primaryResult.outcome match {
+        case lang.Left(_) ⇒
+          val forkDetectionResult = forkDetector.detectForks(
+            primaryResult.trustedState.trustedLightBlock,
+            trustedState.trustedLightBlock,
+            peerList.witnesses.map(verifierBuilder.constructVerifier(_, votingPowerVerifier, trustDuration)).toScala
+          )
+
+          forkDetectionResult match {
+            case ForkDetection.ForkDetected(detected) ⇒
+              val (forks, newPeerList) = processForks(detected)
+
+              if (forks.nonEmpty)
+                (
+                  newPeerList,
+                  Right(new Supervisor.Error() {
+                    override def toString: String = "Error:" + forkDetectionResult.toString
+                  })
+                )
+              else
+                verifyToTarget(height, newPeerList)
+
+            case ForkDetection.NoForks ⇒
+              trustedState = primaryResult.trustedState
+              (peerList, Left(primaryResult.trustedState.trustedLightBlock))
+          }
+        case lang.Right(content) ⇒
+          if (peerList.witnessesIds.isEmpty)
+            (
+              peerList,
+              Right(new Supervisor.Error() {
+                override def toString: String = "Error:" + content.toString
+              })
+            )
+          else
+            verifyToTarget(height, peerList.markPrimaryAsFaulty)
+      }
     }
 
     private def processForks(detected: List[ForkDetection.Fork]): (List[ForkDetection.Forked], PeerList) = {
@@ -43,57 +98,13 @@ object EventLoopClient {
 
       detected.foreach {
         case ForkDetection.Faulty(block) ⇒
-          // TODO might raise an exception if the PeerId is wrong
+          // TODO might raise an exception if the PeerId is wrong, should be fixed
           resultingPeerList = resultingPeerList.markWitnessAsFaulty(block.peerId)
-        case ForkDetection.Forked(primary, witness) ⇒
+        case ForkDetection.Forked(_, _) ⇒
         // TODO report forks
       }
 
       (detected.filter(_.isInstanceOf[Forked]).asInstanceOf, resultingPeerList)
-    }
-
-    private def verifyToTarget(height: Option[Height]): Either[LightBlock, Supervisor.Error] = {
-      while (true) {
-        val primaryVerifier = verifierBuilder.constructVerifier(peerList.primary, votingPowerVerifier, trustDuration)
-
-        val primaryResult =
-          primaryVerifier.verifyUntrusted(
-            trustedState,
-            UntrustedStates.empty(height.getOrElse(peerList.primary.currentHeight)))
-
-        primaryResult.outcome match {
-          case lang.Left(_) ⇒
-            val forkDetectionResult = forkDetector.detectForks(
-              primaryResult.trustedState.trustedLightBlock,
-              trustedState.trustedLightBlock,
-              peerList.witnesses.map(verifierBuilder.constructVerifier(_, votingPowerVerifier, trustDuration)).toScala
-            )
-
-            forkDetectionResult match {
-              // TODO report forks and change witness
-              case ForkDetection.ForkDetected(detected) ⇒
-                val (forks, newPeerList) = processForks(detected)
-                peerList = newPeerList
-
-                if (forks.nonEmpty)
-                  return Right(new Supervisor.Error() {
-                    override def toString: String = "Error:" + forkDetectionResult.toString
-                  })
-
-              case ForkDetection.NoForks ⇒
-                trustedState = primaryResult.trustedState
-                Left(primaryResult.trustedState.trustedLightBlock)
-            }
-          case lang.Right(content) ⇒
-            if (peerList.witnessesIds.isEmpty)
-              return Right(new Supervisor.Error() {
-                override def toString: String = "Error:" + content.toString
-              })
-            peerList = peerList.markPrimaryAsFaulty
-        }
-      }
-
-      throw new RuntimeException("should never happen")
     }
 
   }
