0a472de7697ea40319ab1e9d1cc1dd2e91790ca1
Stevan <ognjanovic.stevan1@gmail.com>
2020-07-06 17:40:23 +0200

    Update single step skipping.

--- light-client/src/test/scala/ch/epfl/ognjanovic/stevan/tendermint/light/VerifierBisectionVerificationTests.scala
+++ light-client/src/test/scala/ch/epfl/ognjanovic/stevan/tendermint/light/VerifierBisectionVerificationTests.scala
@@ -1,6 +1,9 @@
 package ch.epfl.ognjanovic.stevan.tendermint.light
 
-import ch.epfl.ognjanovic.stevan.tendermint.verified.light.VerificationErrors.{ExpiredTrustedState, InsufficientCommitPower}
+import ch.epfl.ognjanovic.stevan.tendermint.verified.light.VerificationErrors.{
+  ExpiredTrustedState,
+  InsufficientCommitPower
+}
 import ch.epfl.ognjanovic.stevan.tendermint.verified.light.VerificationOutcomes._
 import ch.epfl.ognjanovic.stevan.tendermint.verified.types._
 import org.scalatest.flatspec.AnyFlatSpec
@@ -8,8 +11,8 @@ import org.scalatest.flatspec.AnyFlatSpec
 sealed class VerifierBisectionVerificationTests extends AnyFlatSpec {
 
   "Verifying a block with the same validator sets" should "succeed" in {
-    val (verifier, trustedState, provider) = VerifierTests.deserializeSingleStepTestCase(
-      "/single-step/skipping/validator_set/skip_one_block.json")
+    val (verifier, trustedState, provider) =
+      VerifierTests.deserializeSingleStepTestCase("/single-step/skipping/validator_set/skip_one_block.json")
 
     val requestHeight = Height(3)
     val result = verifier.verify(trustedState, provider.lightBlock(requestHeight))
@@ -17,10 +20,9 @@ sealed class VerifierBisectionVerificationTests extends AnyFlatSpec {
     assert(result == Success)
   }
 
-
   "Verifying a block with the same validator sets" should "succeed for height 7" in {
-    val (verifier, trustedState, provider) = VerifierTests.deserializeSingleStepTestCase(
-      "/single-step/skipping/validator_set/skip_five_blocks.json")
+    val (verifier, trustedState, provider) =
+      VerifierTests.deserializeSingleStepTestCase("/single-step/skipping/validator_set/skip_five_blocks.json")
 
     val requestHeight = Height(7)
     val result = verifier.verify(trustedState, provider.lightBlock(requestHeight))
@@ -49,8 +51,8 @@ sealed class VerifierBisectionVerificationTests extends AnyFlatSpec {
   }
 
   "Verifying a block with sufficient commit power" should "succeed for height 3" in {
-    val (verifier, trustedState, provider) = VerifierTests.deserializeSingleStepTestCase(
-      "/single-step/skipping/commit/more_than_two_third_vals_sign.json")
+    val (verifier, trustedState, provider) =
+      VerifierTests.deserializeSingleStepTestCase("/single-step/skipping/commit/more_than_two_third_vals_sign.json")
 
     val requestHeight = Height(3)
     val result = verifier.verify(trustedState, provider.lightBlock(requestHeight))
@@ -59,8 +61,8 @@ sealed class VerifierBisectionVerificationTests extends AnyFlatSpec {
   }
 
   "Verifying a block with insufficient commit power" should "recognize an invalid commit for height 3" in {
-    val (verifier, trustedState, provider) = VerifierTests.deserializeSingleStepTestCase(
-      "/single-step/skipping/commit/one_third_vals_dont_sign.json")
+    val (verifier, trustedState, provider) =
+      VerifierTests.deserializeSingleStepTestCase("/single-step/skipping/commit/one_third_vals_dont_sign.json")
 
     val requestHeight = Height(3)
     val result = verifier.verify(trustedState, provider.lightBlock(requestHeight))
@@ -69,10 +71,10 @@ sealed class VerifierBisectionVerificationTests extends AnyFlatSpec {
   }
 
   "Verification with an expired trusted header" should "fail with expired trusted state" in {
-    val (verifier, trustedState, provider) = VerifierTests.deserializeSingleStepTestCase(
-      "/single-step/skipping/header/out_of_trusting_period.json")
+    val (verifier, trustedState, provider) =
+      VerifierTests.deserializeSingleStepTestCase("/single-step/skipping/header/out_of_trusting_period.json")
 
-    val requestHeight = Height(5)
+    val requestHeight = Height(6)
     val result = verifier.verify(trustedState, provider.lightBlock(requestHeight))
 
     assert(result.asInstanceOf[Failure].reason == ExpiredTrustedState)
