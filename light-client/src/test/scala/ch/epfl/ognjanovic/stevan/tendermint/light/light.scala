bc9ec7f7b320e7964d634887346877c254d893d2
Stevan <ognjanovic.stevan1@gmail.com>
2020-07-07 10:54:46 +0200

    First part of test refactoring.

--- light-client/src/test/scala/ch/epfl/ognjanovic/stevan/tendermint/light/light.scala
+++ light-client/src/test/scala/ch/epfl/ognjanovic/stevan/tendermint/light/light.scala
@@ -2,7 +2,7 @@ package ch.epfl.ognjanovic.stevan.tendermint
 
 import java.time.Instant
 
-import ch.epfl.ognjanovic.stevan.tendermint.rpc.circe.{circe, CirceDecoders}
+import ch.epfl.ognjanovic.stevan.tendermint.rpc.circe.circe
 import ch.epfl.ognjanovic.stevan.tendermint.rpc.types.SignedHeader
 import ch.epfl.ognjanovic.stevan.tendermint.verified.light.LightBlockProviders.LightBlockProvider
 import ch.epfl.ognjanovic.stevan.tendermint.verified.light.TrustedStates.{SimpleTrustedState, TrustedState}
@@ -10,53 +10,48 @@ import ch.epfl.ognjanovic.stevan.tendermint.verified.light.VotingPowerVerifiers
 import ch.epfl.ognjanovic.stevan.tendermint.verified.light.VotingPowerVerifiers.VotingPowerVerifier
 import ch.epfl.ognjanovic.stevan.tendermint.verified.types.{Duration, Height, LightBlock, ValidatorSet}
 import io.circe.Decoder
+import io.circe.generic.semiauto._
 
 package object light {
+
+  implicit val lightBlockProviderDecoder: Decoder[LightBlockProvider] =
+    InMemoryProvider.defaultChainDecoder(VerifierTests.lightBlockDecoder)
+
+  case class TrustedInitialState(
+    signed_header: SignedHeader,
+    next_validator_set: ValidatorSet,
+    now: Instant,
+    trusting_period: Long)
+
+  case class SingleStepTestCase(initial: TrustedInitialState, input: LightBlockProvider)
+
+  implicit val trustedInitialStateDecoder: Decoder[TrustedInitialState] = deriveDecoder
+
+  implicit val singleStepTestCase: Decoder[SingleStepTestCase] = deriveDecoder
 
   implicit val singleStepTestCaseDecoder
     : Decoder[(TrustedState, VotingPowerVerifier, Duration, Instant, LightBlockProvider)] =
     cursor =>
       for {
-        signedHeader <-
-          cursor
-            .downField("initial")
-            .downField("signed_header")
-            .as[SignedHeader](CirceDecoders.signedHeaderDecoder)
-        nextValidatorSet <-
-          cursor
-            .downField("initial")
-            .downField("next_validator_set")
-            .as[ValidatorSet](CirceDecoders.conformanceTestValidatorSetDecoder)
-        now <-
-          cursor
-            .downField("initial")
-            .downField("now")
-            .as[Instant](circe.instantDecoder)
-        trustingPeriod <-
-          cursor
-            .downField("initial")
-            .downField("trusting_period")
-            .as[Long]
-        provider <-
-          cursor
-            .downField("input")
-            .as[LightBlockProvider](InMemoryProvider.defaultChainDecoder(VerifierTests.lightBlockDecoder))
+        testCase ← cursor.as[SingleStepTestCase]
       } yield {
         val trustVerifier = VotingPowerVerifiers.defaultTrustVerifier
 
         (
           SimpleTrustedState(
             LightBlock(
-              signedHeader.header,
-              signedHeader.commit,
-              nextValidatorSet,
-              nextValidatorSet,
-              VerifierTests.defaultProvider),
-            trustVerifier),
+              testCase.initial.signed_header.header,
+              testCase.initial.signed_header.commit,
+              testCase.initial.next_validator_set,
+              testCase.initial.next_validator_set,
+              VerifierTests.defaultProvider
+            ),
+            trustVerifier
+          ),
           trustVerifier,
-          Duration(0, trustingPeriod),
-          now,
-          provider)
+          Duration(0, testCase.initial.trusting_period),
+          testCase.initial.now,
+          testCase.input)
       }
 
   implicit val multiStepTestCaseDecoder: Decoder[(TrustOptions, LightBlockProvider, Height, Instant)] = cursor =>
