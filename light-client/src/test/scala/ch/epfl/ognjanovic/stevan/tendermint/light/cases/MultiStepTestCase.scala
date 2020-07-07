62763b1f97d320e964e015540fbb733d7f53128d
Stevan <ognjanovic.stevan1@gmail.com>
2020-07-07 15:30:19 +0200

    Tests with peer input format.

--- light-client/src/test/scala/ch/epfl/ognjanovic/stevan/tendermint/light/cases/MultiStepTestCase.scala
+++ light-client/src/test/scala/ch/epfl/ognjanovic/stevan/tendermint/light/cases/MultiStepTestCase.scala
@@ -2,21 +2,24 @@ package ch.epfl.ognjanovic.stevan.tendermint.light.cases
 
 import java.time.Instant
 
-import ch.epfl.ognjanovic.stevan.tendermint.light.{InMemoryProvider, VerifierTests}
-import ch.epfl.ognjanovic.stevan.tendermint.verified.light.LightBlockProviders.LightBlockProvider
+import ch.epfl.ognjanovic.stevan.tendermint.light.cases.MultiStepTestCase.PeerInput
+import ch.epfl.ognjanovic.stevan.tendermint.rpc.circe.circe.instantDecoder
 import io.circe._
 import io.circe.generic.semiauto._
+import TrustOptions.decoder
+import InputFormat.inputFormatDecoder
+import ch.epfl.ognjanovic.stevan.tendermint.rpc.circe.CirceDecoders.{
+  conformanceTestValidatorSetDecoder,
+  signedHeaderDecoder
+}
 
-case class MultiStepTestCase(
-  trust_options: TrustOptions,
-  primary: LightBlockProvider,
-  height_to_verify: Long,
-  now: Instant)
+case class MultiStepTestCase(trust_options: TrustOptions, primary: PeerInput, height_to_verify: Long, now: Instant)
 
 object MultiStepTestCase {
 
-  implicit private val lightBlockProviderDecoder: Decoder[LightBlockProvider] =
-    InMemoryProvider.decoder(VerifierTests.lightBlockDecoder)
+  case class PeerInput(chain_id: String, lite_blocks: Array[InputFormat])
+
+  implicit private val peerInputDecoder: Decoder[PeerInput] = deriveDecoder
 
   implicit val decoder: Decoder[MultiStepTestCase] = deriveDecoder
 }
